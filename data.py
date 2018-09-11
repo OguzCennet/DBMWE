edddc37dee01deaca9bd460b3b7a41670f607630
Cennet Oguz <st154489@stud.uni-stuttgart.de>
2018-09-11 16:26:27 +0200

    Added Keras framework

--- data.py
+++ data.py
@@ -3,18 +3,21 @@ import numpy as np
 class DataPreparation:
 	def __init__(self, dataset):
 		self.dataset = dataset
+		self.word2id = self.get_word2id()
 		self.onehotvectors = self.get_onehotvector()
 
-	def get_onehotvector(self):
-		print("get_onehotvector")
+	def get_word2id(self):
 		word_list = list()
 		for sentence in self.dataset.sentences:
 			word_list+= sentence.lemma_list
 		unique_words = list(set(word_list))
-		word2id = dict((c, i) for i, c in enumerate(unique_words))
-		onehot_matrix = np.zeros((len(word2id), len(word2id)),  dtype = int)
-		for word, id in word2id.items():
+		return dict((c, i) for i, c in enumerate(unique_words))
+
+	def get_onehotvector(self):
+		onehot_matrix = np.zeros((len(self.word2id), len(self.word2id)),  dtype = int)
+		for word, id in self.word2id.items():
 			onehot_matrix[id][id] = 1
+		return onehot_matrix
 
 
 
--- project.py
+++ project.py
@@ -1,9 +1,19 @@
 from i_o import Dataset, Output
 from data import DataPreparation
+from keras.models import Model
+from keras.layers import Input, Dense, Reshape, merge
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.sequence import skipgrams
+from keras.preprocessing import sequence
 
-de_corpora = "../dataset/de_pud-ud-test.conllu"
-tr_corpora = "../dataset/tr_pud-ud-test.conllu"
-en_corpora = "../dataset/en_pud-ud-test.conllu"
+import urllib
+import collections
+import os
+import zipfile
+
+de_corpora = "dataset/de_pud-ud-test.conllu"
+tr_corpora = "dataset/tr_pud-ud-test.conllu"
+en_corpora = "dataset/en_pud-ud-test.conllu"
 
 
 def get_data():
